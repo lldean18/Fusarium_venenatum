4b7fdcac2d76ffff718fd675065ca29c56737301
Laura Dean <lldean18@gmail.com>
2026-09-10 13:18:20 +0100

    final filtering file for vcf

---
+++ 11_filter_variants.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# 10/9/26
+
+# script to filter raw variants
+
+#setup env
+module load bcftools-uoneasy/1.19-GCC-13.2.0
+module load singularity/3.8.5
+#cd ~/software_bin/singularity
+#singularity build gatk.sif docker://broadinstitute/gatk:latest
+WKDIR=/gpfs01/home/mbzlld/data/paul_dyer/variants
+cd /gpfs01/home/mbzlld/data/paul_dyer/variants
+
+
+####################################################
+# apply gatk best practice site level hard filters #
+####################################################
+
+# first mark the reads that pass the filters with PASS for the snps
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk VariantFiltration \
+--variant FusVen.raw.snps.vcf.gz \
+-filter "QD < 2.0" --filter-name "QD2" \
+-filter "QUAL < 30.0" --filter-name "QUAL30" \
+-filter "SOR > 3.0" --filter-name "SOR3" \
+-filter "FS > 60.0" --filter-name "FS60" \
+-filter "MQ < 40.0" --filter-name "MQ40" \
+-filter "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
+-filter "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
+--output FusVen.raw.snps.SFmarked.vcf.gz
+
+# then retain only reads with PASS for the snps
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk SelectVariants \
+--variant FusVen.raw.snps.SFmarked.vcf.gz \
+--exclude-filtered true \
+--output FusVen.raw.snps.SF.vcf.gz
+
+# first mark the reads that pass the filters with PASS for the indels
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk VariantFiltration \
+--variant FusVen.raw.indels.vcf.gz \
+-filter "QD < 2.0" --filter-name "QD2" \
+-filter "QUAL < 30.0" --filter-name "QUAL30" \
+-filter "FS > 200.0" --filter-name "FS200" \
+-filter "ReadPosRankSum < -20.0" --filter-name "ReadPosRankSum-20" \
+--output FusVen.raw.indels.marked.vcf.gz
+
+# then retain only reads with PASS for the indels
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk SelectVariants \
+--variant FusVen.raw.indels.marked.vcf.gz \
+--exclude-filtered true \
+--output FusVen.raw.indels.filtered.vcf.gz
+
+
+
+# check how many variants were removed by site-level filtering in the vcf
+bcftools view -H FusVen.raw.snps.vcf.gz | wc -l # 2,274,351
+bcftools view -H FusVen.raw.snps.SF.vcf.gz | wc -l # 2,227,569
+
+# then check how many indels were removed by filtering
+bcftools view -H FusVen.raw.indels.vcf.gz | wc -l # 238,651
+bcftools view -H FusVen.raw.indels.filtered.vcf.gz | wc -l # 237,897
+
+
+
+##############################################
+# Apply genotype-level filters with bcftools #
+##############################################
+
+# first check the annotations I want to use are present in the vcf
+bcftools view -h FusVen.raw.snps.SF.vcf.gz | grep -E 'ID=(QD|MQ|FS|SOR|MQRankSum|ReadPosRankSum|ExcessHet)'
+
+# set genotypes to missing if they have a depth (DP) <10 or a genotype quality (GQ) <20
+bcftools +setGT \
+    FusVen.raw.snps.SF.vcf.gz \
+    -Oz \
+    -o FusVen.raw.snps.SF.GF1set.vcf.gz \
+    -- \
+    -t q \
+    -n . \
+    -i 'FMT/DP<10 || FMT/GQ<20'
+bcftools index -t FusVen.raw.snps.SF.GF1set.vcf.gz
+
+# set heterozygotes to missing if their allele counts are statistically inconsistent with a 50:50 expectation at P <0.05.
+bcftools +setGT \
+    FusVen.raw.snps.SF.GF1set.vcf.gz \
+    -Oz \
+    -o FusVen.raw.snps.SF.GFset.vcf.gz \
+    -- \
+    -t 'b:AD<0.05' \
+    -n .
+bcftools index -t FusVen.raw.snps.SF.GFset.vcf.gz
+
+# reset the missingness tags
+bcftools +fill-tags \
+    FusVen.raw.snps.SF.GFset.vcf.gz \
+    -Oz \
+    -o FusVen.raw.snps.SF.GFset.miss.vcf.gz \
+    -- -t F_MISSING,MAF,AF,AC,AN
+bcftools index -t FusVen.raw.snps.SF.GFset.miss.vcf.gz
+
+# now filter for missingness
+bcftools view \
+    -i 'INFO/F_MISSING<=0.20' \
+    FusVen.raw.snps.SF.GFset.miss.vcf.gz \
+    -Oz \
+    -o FusVen.snps.filtered.vcf.gz
+bcftools index -t FusVen.snps.filtered.vcf.gz
+
+# check how many variants were filtered out by genotype-filtering
+bcftools view -H FusVen.raw.snps.SF.vcf.gz | wc -l # 2,227,569
+bcftools view -H FusVen.snps.filtered.vcf.gz | wc -l # 1,709,395
+
+# remove the unecessary intermediate files
+rm FusVen.raw.snps.SF.GFset.vcf.gz*
+rm FusVen.raw.snps.SF.GF1set.vcf.gz*
+
+
