592be3c83246711e9de560bf51396a37be5004d7
Laura Dean <lldean18@gmail.com>
2026-09-10 15:49:10 +0100

    updating to run again on the haploid calls

--- 10_joint_genotype.sh
+++ 10_joint_genotype.sh
@@ -21,59 +21,58 @@ cd ~/data/paul_dyer
 WKDIR=/gpfs01/home/mbzlld/data/paul_dyer
 
 
-# hashing this bit out because it ran successfully
-##  # consolidate gvcfs into a database
-##  echo "MAKING THE DATABASE"
-##  singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk GenomicsDBImport \
-##  --genomicsdb-workspace-path variants/FusariumDB \
-##  --variant variants/114-1.g.vcf.gz \
-##  --variant variants/114-2.g.vcf.gz \
-##  --variant variants/114-3.g.vcf.gz \
-##  --variant variants/114-4.g.vcf.gz \
-##  --variant variants/114-5.g.vcf.gz \
-##  --variant variants/114-6.g.vcf.gz \
-##  --variant variants/114-7.g.vcf.gz \
-##  --variant variants/114-8.g.vcf.gz \
-##  --variant variants/114-9.g.vcf.gz \
-##  --variant variants/114-10.g.vcf.gz \
-##  --variant variants/114-11.g.vcf.gz \
-##  --variant variants/114-12.g.vcf.gz \
-##  --variant variants/114-13.g.vcf.gz \
-##  --variant variants/114-14.g.vcf.gz \
-##  --variant variants/114-15.g.vcf.gz \
-##  --variant variants/114-16.g.vcf.gz \
-##  --variant variants/114-17.g.vcf.gz \
-##  --variant variants/114-18.g.vcf.gz \
-##  --variant variants/114-19.g.vcf.gz \
-##  --variant variants/114-20.g.vcf.gz \
-##  --variant variants/114-21.g.vcf.gz \
-##  --variant variants/114-22.g.vcf.gz \
-##  --variant variants/114-23.g.vcf.gz \
-##  --variant variants/114-24.g.vcf.gz \
-##  --variant variants/114-25.g.vcf.gz \
-##  --reference reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna \
-##  --intervals NC_038015.1 \
-##  --intervals NC_038016.1 \
-##  --intervals NC_038012.1 \
-##  --intervals NC_038013.1 \
-##  --intervals NC_038014.1 \
-##  --intervals NW_020311997.1 \
-##  --reader-threads 8
-##  echo "FINISHED MAKING THE DATABASE"
-##  #119-01.g.vcf.gz
-##  #120-01.g.vcf.gz
+# consolidate gvcfs into a database
+echo "MAKING THE DATABASE"
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk GenomicsDBImport \
+--genomicsdb-workspace-path variants/FusariumDB \
+--variant variants/114-1.g.vcf.gz \
+--variant variants/114-2.g.vcf.gz \
+--variant variants/114-3.g.vcf.gz \
+--variant variants/114-4.g.vcf.gz \
+--variant variants/114-5.g.vcf.gz \
+--variant variants/114-6.g.vcf.gz \
+--variant variants/114-7.g.vcf.gz \
+--variant variants/114-8.g.vcf.gz \
+--variant variants/114-9.g.vcf.gz \
+--variant variants/114-10.g.vcf.gz \
+--variant variants/114-11.g.vcf.gz \
+--variant variants/114-12.g.vcf.gz \
+--variant variants/114-13.g.vcf.gz \
+--variant variants/114-14.g.vcf.gz \
+--variant variants/114-15.g.vcf.gz \
+--variant variants/114-16.g.vcf.gz \
+--variant variants/114-17.g.vcf.gz \
+--variant variants/114-18.g.vcf.gz \
+--variant variants/114-19.g.vcf.gz \
+--variant variants/114-20.g.vcf.gz \
+--variant variants/114-21.g.vcf.gz \
+--variant variants/114-22.g.vcf.gz \
+--variant variants/114-23.g.vcf.gz \
+--variant variants/114-24.g.vcf.gz \
+--variant variants/114-25.g.vcf.gz \
+--reference reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna \
+--intervals NC_038015.1 \
+--intervals NC_038016.1 \
+--intervals NC_038012.1 \
+--intervals NC_038013.1 \
+--intervals NC_038014.1 \
+--intervals NW_020311997.1 \
+--reader-threads 8
+echo "FINISHED MAKING THE DATABASE"
+#119-01.g.vcf.gz
+#120-01.g.vcf.gz
 
 
-##  # perform joint genotype calling
-##  echo "PERFORMING JOINT GENOTYPE CALLING"
-##  singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk GenotypeGVCFs \
-##  --output variants/FusVen.raw.vcf.gz \
-##  --reference reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna \
-##  --variant gendb://variants/FusariumDB \
-##  --annotation-group StandardAnnotation \
-##  --annotation-group StandardHCAnnotation \
-##  --call-genotypes true
-##  echo "FINIHSED JOINT GENOTYPE CALLING"
+# perform joint genotype calling
+echo "PERFORMING JOINT GENOTYPE CALLING"
+singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk GenotypeGVCFs \
+--output variants/FusVen.raw.vcf.gz \
+--reference reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna \
+--variant gendb://variants/FusariumDB \
+--annotation-group StandardAnnotation \
+--annotation-group StandardHCAnnotation \
+--call-genotypes true
+echo "FINIHSED JOINT GENOTYPE CALLING"
 
 
 #singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk SelectVariants --help
