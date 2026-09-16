ff685c8cc1c368fbd932d6796bd01fdfdc0148bf
Laura Dean <lldean18@gmail.com>
2026-09-16 14:16:04 +0100

    updating

--- 14_scaffold_newer_assembly.sh
+++ 14_scaffold_newer_assembly.sh
@@ -9,8 +9,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=30g
-#SBATCH --time=12:00:00
+#SBATCH --mem=10g
+#SBATCH --time=1:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # setup env
@@ -22,10 +22,15 @@ reference=GCF_900007375.1_ASM90000737v1_genomic.fna
 
 
 # scaffold assembly
-ragtag.py scaffold -t 16 -o ${assembly%.*}_ragtag $reference $assembly
+# -r = infer gap sizes
+ragtag.py scaffold \
+-t 16 \
+-r \
+-o ${assembly%.*}_ragtag \
+$reference $assembly
 
 # get rid of the ragtag suffixes
-sed -i 's/_RagTag//' ${assembly%.*}_ragtag/ragtag.scaffold.fasta
+#sed -i 's/_RagTag//' ${assembly%.*}_ragtag/ragtag.scaffold.fasta
 
 
 # cleanup env
