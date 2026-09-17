406716e9699331b1e90c868b0e1fb6775f01ca10
Laura Dean <lldean18@gmail.com>
2026-09-17 12:05:34 +0100

    updating to run on a different assembly

--- 12_count_snps_in_windows.sh
+++ 12_count_snps_in_windows.sh
@@ -4,13 +4,17 @@
 # script to count SNPs in windows for plotting as a heatmap
 
 # setup env
-cd /gpfs01/home/mbzlld/data/paul_dyer
+#reference=/gpfs01/home/mbzlld/data/paul_dyer/reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna
+#genome_identifier=ASM90000737v1
+reference=/gpfs01/home/mbzlld/data/paul_dyer/reference_genomes/GCF_020744135.1_Fusven1_genomic.fna
+genome_identifier=Fusven1
+cd /gpfs01/home/mbzlld/data/paul_dyer/$genome_identifier
 mkdir -p snp_density
 
 # generate windows
-bedtools makewindows -g reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna.fai -w 100000 > snp_density/windows_100kb.bed
-bedtools makewindows -g reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna.fai -w 50000 > snp_density/windows_50kb.bed
-bedtools makewindows -g reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna.fai -w 20000 > snp_density/windows_20kb.bed
+bedtools makewindows -g $reference.fai -w 100000 > snp_density/windows_100kb.bed
+bedtools makewindows -g $reference.fai -w 50000 > snp_density/windows_50kb.bed
+bedtools makewindows -g $reference.fai -w 20000 > snp_density/windows_20kb.bed
 
 #######################
 # count snps in windows
@@ -36,12 +40,12 @@ bedtools coverage \
 ########################
 
 # filter the gff to retain only genes
-awk -F'\t' '$3 == "gene"' reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.gff > reference_genomes/GCF_900007375.1_ASM90000737v1_genomic_genes.gff
+awk -F'\t' '$3 == "gene"' ${reference%.*}.gff > ${reference%.*}_genes.gff
 
 # count the genes across windows
 bedtools coverage \
     -a snp_density/windows_20kb.bed \
-    -b reference_genomes/GCF_900007375.1_ASM90000737v1_genomic_genes.gff \
+    -b ${reference%.*}_genes.gff \
     -counts \
     > snp_density/gene_counts_20kb_winds.txt
 
