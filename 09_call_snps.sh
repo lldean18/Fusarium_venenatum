1d1e64999940dfc0dbc4e9df6be608242351d157
Laura Dean <lldean18@gmail.com>
2026-09-17 10:59:32 +0100

    updating to make reference genome specification easier

--- 09_call_snps.sh
+++ 09_call_snps.sh
@@ -25,21 +25,22 @@ echo "filtering mapped reads for sample $SAMPLE"
 module load singularity/3.8.5
 #cd ~/software_bin/singularity
 #singularity build gatk.sif docker://broadinstitute/gatk:latest
-genome_identifier=Fusven1
-mkdir -p ~/data/paul_dyer/$genome_identifier/variants
 cd ~/data/paul_dyer
 WKDIR=/gpfs01/home/mbzlld/data/paul_dyer
+#reference=GCF_900007375.1_ASM90000737v1_genomic.fna
+reference=GCF_020744135.1_Fusven1_genomic.fna
+genome_identifier=Fusven1
+mkdir -p ~/data/paul_dyer/$genome_identifier/variants
 
 
 # index the reference
-#singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk CreateSequenceDictionary -R reference_genomes/GCF_900007375.1_ASM90000737v1_genomic.fna
-#singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk CreateSequenceDictionary -R reference_genomes/GCF_020744135.1_Fusven1_genomic.fna
+#singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk CreateSequenceDictionary -R reference_genomes/$reference
 
 
 singularity exec -B ${WKDIR}:${WKDIR} ~/software_bin/singularity/gatk.sif gatk HaplotypeCaller \
 --input $genome_identifier/filtered_bams/$SAMPLE.bam \
 --output $genome_identifier/variants/$SAMPLE.g.vcf.gz \
---reference reference_genomes/GCF_020744135.1_Fusven1_genomic.fna \
+--reference reference_genomes/$reference \
 --emit-ref-confidence GVCF \
 --native-pair-hmm-threads 16 \
 --ploidy 1
